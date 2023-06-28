3c7dd931f931a7631db9436fc84b90adba3da1dc
Bogdan <bod555@ukr.net>
2023-06-28 16:15:55 +0300

    fixed comments themes

--- src/components/widgets/FastComments.tsx
+++ src/components/widgets/FastComments.tsx
@@ -1,14 +1,19 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import { FastCommentsCommentWidget } from "fastcomments-react"
+import { useTheme } from "next-themes"
 
 const FastComment: React.FC = () => {
+  const { theme } = useTheme()
+  const [dark, setDark] = useState(theme === "dark")
+  useEffect(() => setDark(theme === "dark"), [theme])
   const config = {
     tenantId: "demo",
     region: "eu",
     pageId: "native-test",
   }
-  return <FastCommentsCommentWidget tenantId="demo" />
+  return <FastCommentsCommentWidget hasDarkBackground={dark} {...config} />
 }
 
 export default FastComment
